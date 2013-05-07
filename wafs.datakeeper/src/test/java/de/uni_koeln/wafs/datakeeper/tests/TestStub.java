58cef3e032453f4c04d7d8532ff954290b6c7fa8
claesn <c.neuefeind@uni-koeln.de>
2013-05-07 16:33:23 +0200

    Tests for data storage, indexing, searching.

--- wafs.datakeeper/src/test/java/de/uni_koeln/wafs/datakeeper/tests/TestStub.java
+++ wafs.datakeeper/src/test/java/de/uni_koeln/wafs/datakeeper/tests/TestStub.java
@@ -6,7 +6,14 @@ import java.util.List;
 
 import javax.xml.bind.JAXBException;
 
+import junit.framework.Assert;
+
 import org.apache.log4j.Logger;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.util.Version;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -19,25 +26,41 @@ public class TestStub {
 	private static final File mp3Dir = new File("target/mp3s");
 	private static final File luceneDir = new File("target/lucene");
 	private static Logger logger = Logger.getLogger(TestStub.class);
+	private static List<Track> tracks;
 	
 	@BeforeClass
 	public static void initialize() throws JAXBException, IOException {
-		List<Track> tracks = DummyTrackGenerator.createDummyTracks(mp3Dir);
-		Index index = new Index(luceneDir);
-		int indexed = 0;
-		for (Track track : tracks) {
-			if(index.findTrack(track.getLocation()) == null) {
-				index.add(track);
-				indexed++;
-			}
-		}
-		index.commit();
-		logger.info("Indexed " + indexed + " new files, index now contains " + index.getSize() + " tracks.");
+		tracks = DummyTrackGenerator.createDummyTracks(mp3Dir);
 	}
 
 	@Test
-	public void test() {
-		// TODO: Write some tests!
+	public void testAddTracks() throws Exception{
+		DemoIndex index = createDemoIndex();
+		int size = index.getSize();
+		Assert.assertEquals(tracks.size(), size);
 	}
-	
+
+	private DemoIndex createDemoIndex() throws IOException {
+		DemoIndex index = new DemoIndex();
+		for (Track track: tracks){
+			index.add(track);
+		}
+		index.commit();
+		return index;
+	}
+
+	@Test
+	public void testQuery() throws Exception{
+		DemoIndex index = createDemoIndex();
+		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_42);
+		QueryParser parser = new QueryParser(Version.LUCENE_42, "", analyzer);
+		Query query = parser.parse("artist: Herbie Hancock");
+		List<Track> result = index.query(query);
+		Assert.assertNotNull(result);
+		Assert.assertTrue(result.size() > 0);
+		for(Track track : result){
+			System.out.println("Track: " + track);
+		}
+	}
+
 }
